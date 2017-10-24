deb461baf163ba6c362cd74d6aaaad948bff5c59
Martin Davis <martindavis@exosite.com>
2017-10-24 18:48:36 +0000

    switching back to something deployable by MuranoCLI. Added Logo to application. Added rough provisioning to simulator.

--- simulator/tiny_dirty_430_sim.py
+++ simulator/tiny_dirty_430_sim.py
@@ -5,41 +5,60 @@ import requests
 import random
 import time
 
+product_id = "g1ugjd0cp01ts0000"
+identity = "00:11:22:33:44:55" + str(time.time())
+print(identity)
 
-sn = "00:11:22:33:44:55"
-token = "nrZ0Kd1XVDYIfa4Y7Kra7NICK9DVdoXFgeEvNRJ9"
-# token = "0jqojEzgghmYpKGktHneg6eHhu5b9ner8Bt6C9kX"
-device_api_host = "https://g1ugjd0cp01ts0000.m2.exosite.io/onep:v1/stack/alias"
+activate_api_host = "https://"+product_id+".m2.exosite.io/provision/activate"
+device_api_host = "https://"+product_id+".m2.exosite.io/onep:v1/stack/alias"
+
+activate_headers = {
+    'user-agent': 'quick_simplelink_sim', 
+    'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8'
+}
+
+activate_payload = {'id': identity}
+
+print("Making activate request")
+resp = requests.post(activate_api_host, headers = activate_headers, data = activate_payload)
+print(resp.status_code)
+print(resp.text)
+
+token = resp.text
 
 write_headers = {
-    'user-agent': 'tiny_dirty_430_sim', 
+    'user-agent': 'quick_simplelink_sim', 
     'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
     'X-Exosite-CIK': token
-    }
+}
 read_headers = {
-    'user-agent': 'tiny_dirty_430_sim',
+    'user-agent': 'quick_simplelink_sim',
     'Accept': 'application/x-www-form-urlencoded; charset=utf-8',
     'X-Exosite-CIK': token
-    }
+}
 
-led_reset_payload = {'ledd1': 0}
-
-print("Reseting LED state.")
+print("Resetting LED state.")
+led_reset_payload = {'led': 'off'}
 resp = requests.post(device_api_host, headers = write_headers, data = led_reset_payload)
 print(resp.status_code)
 print(resp.text)
 
 loop_start_time = time.time()
 
+
+print("Starting main loop to write to SimpleLink aliases")
 while True:
     time.sleep(1)
     # generate some dirty data
-    random.seed()
+    random.seed(time.time())
     switch_one_count = random.randint(1,75)
-    random.SystemRandom()
+
+    random.seed(time.time() + switch_one_count)
     switch_two_count = random.randint(1,10)
-    random.SystemRandom()
-    junc_temp = switch_one_count = random.randint(-40,100)
+
+    random.seed(time.time() + switch_two_count)
+    junc_temp = random.randint(-40,100)
+
     ontime = time.time() - loop_start_time
 
     device_report = {
@@ -54,38 +73,9 @@ while True:
     print(resp.text) 
 
     print("Fetching LED state.")
-    resp = requests.get(device_api_host + "?ledd1", headers = read_headers)
+    resp = requests.get(device_api_host + "?led", headers = read_headers)
     print(resp.status_code)
     print(resp.text)     
     time.sleep(4)
 
-
-
 print("For some reason this program exited.")
-
-# ---
-#   usrsw1:
-#     allowed: []
-#     format: number
-#     settable: false
-#     unit: ''
-#   usrsw2:
-#     allowed: []
-#     format: number
-#     settable: false
-#     unit: ''
-#   temp:
-#     allowed: []
-#     format: number
-#     settable: false
-#     unit: ''
-#   ontime:
-#     allowed: []
-#     format: number
-#     settable: false
-#     unit: ''
-#   led:
-#     allowed: []
-#     format: string
-#     settable: true
-#     aunit: ''
